0d56d178f70dcf891e3a6db5964e76ef6b592877
ArtificalImaginings <137360154+ArtificalImaginings@users.noreply.github.com>
2023-07-01 17:52:01 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 API_KEY = "PKZZOGAN2C7GGYZ11SPW"
-SECRET_KEY = "PKZZOGAN2C7GGYZ11SPW"
+SECRET_KEY = "Artificialg"
 
-ALPACA_CONFIG = [
+ALPACA_CONFIG = [ALPACA_CONFIG
     
 ]
 # Put your own Alpaca key here:
-"API_KEY": "PKZZOGAN2C7GGYZ11SPW",
+"API_KEY": [PKZZOGAN2C7GGYZ11SPW],
 # Put your own Alpaca secret here:
-"API_SECRET": "Alpaca Login",
+"API_SECRET": [AlpacaLogin],
 # If you want to go live, you must change this
-"ENDPOINT": "www.gmail.com"
+"ENDPOINT": [www.gmail.com]
 

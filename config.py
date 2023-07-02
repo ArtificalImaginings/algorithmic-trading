1d60eff39fc58f5fb5584bbb80ceab5c00fc6306
ArtificalImaginings <137360154+ArtificalImaginings@users.noreply.github.com>
2023-07-02 00:00:00 -0400

    g
    gg

--- config.py
+++ config.py
@@ -5,7 +5,14 @@ ALPACA_CONFIG = [
     
 ]
 # Put your own Alpaca key here:
+<<<<<<< Updated upstream
 "API_KEY": "PKZZOGAN2C7GGYZ11SPW",
+=======
+def ():
+    return [PKZZOGAN2C7GGYZ11SPW]
+
+"API_KEY": new_func(),
+>>>>>>> Stashed changes
 # Put your own Alpaca secret here:
 "API_SECRET": "Alpaca Login",
 # If you want to go live, you must change this
